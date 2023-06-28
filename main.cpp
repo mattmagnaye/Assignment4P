af76970b1c20825af0ba8a9aca909f4341e93113
mattaaronmagnaye <123015615+mattaaronmagnaye@users.noreply.github.com>
2023-06-27 22:25:28 -0700

    added containsPair function

---
+++ main.cpp
@@ -0,0 +1,50 @@
+/*
+* Program: Poker (sort of) - Project 13.1 (Assignment 4P: Arrays)
+* Author: Matt Aaron Magnaye
+* Instructor: Professor David Harden
+* Class: CIS 278
+* Date: 06/27/2023
+* FIle: main.cpp
+* Description: It is a program that reads HAND_SIZE cards from the user, 
+*              then analyzes the cards and prints out the type of poker 
+*              hand that they represent.
+*
+*/
+#include <iostream>
+using namespace std;
+
+// Declaring necessary constants and values per as instruction
+const int HAND_SIZE = 5;
+const int LOWEST_NUM = 2;
+const int HIGHEST_NUM = 9;
+
+
+int main() {
+
+    int hand[HAND_SIZE];
+    cout << "Enter " << HAND_SIZE << " numeric cards, no face cards. Use " << LOWEST_NUM << " - " << HIGHEST_NUM << ".\n";
+
+    for (int i = 0; i < HAND_SIZE; ++i) {
+        cout << "Card " << (i+1) << ": ";
+        cin >> hand[i];
+    }
+
+    
+
+    return 0
+}
+
+bool containsPair(const int hand[]) {
+    map<int, int> cardCount;
+    for(int i = 0; i < HAND_SIZE; i++) {
+        cardCount[hand[i]]++;
+    }
+
+    for(auto it : cardCount)  {
+        if(it.second == 2) {
+            return true;
+        }
+    }
+    return false;
+
+}
