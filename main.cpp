ea5e5e3e581b8aeb053e9b62e0027af956018ca8
mattaaronmagnaye <123015615+mattaaronmagnaye@users.noreply.github.com>
2023-06-27 23:09:00 -0700

    trying extra-credit

--- main.cpp
+++ main.cpp
@@ -1,23 +1,13 @@
-/*
-* Program: Poker (sort of) - Project 13.1 (Assignment 4P: Arrays)
-* Author: Matt Aaron Magnaye
-* Instructor: Professor David Harden
-* Class: CIS 278
-* Date: 06/27/2023
-* FIle: main.cpp
-* Description: It is a program that reads HAND_SIZE cards from the user, 
-*              then analyzes the cards and prints out the type of poker 
-*              hand that they represent.
-*
-*/
 #include <iostream>
 #include <map>
 using namespace std;
 
-// Declaring necessary constants and values per as instruction
 const int HAND_SIZE = 5;
 const int LOWEST_NUM = 2;
 const int HIGHEST_NUM = 9;
+
+void populateCardCountMap(const int hand[], map<int, int>& cardCount);
+int countOccurrences(const map<int, int>& cardCount, int count);
 
 bool containsPair(const int hand[]);
 bool containsTwoPair(const int hand[]);
@@ -26,9 +16,7 @@ bool containsStraight(const int hand[]);
 bool containsFullHouse(const int hand[]);
 bool containsFourOfaKind(const int hand[]);
 
-
 int main() {
-
     int hand[HAND_SIZE];
     cout << "Enter " << HAND_SIZE << " numeric cards, no face cards. Use " << LOWEST_NUM << " - " << HIGHEST_NUM << ".\n";
 
@@ -39,20 +27,15 @@ int main() {
 
     if (containsFourOfaKind(hand)) {
         cout << "Four of a kind!";
-    }
-    else if (containsFullHouse(hand)) {
+    } else if (containsFullHouse(hand)) {
         cout << "Full House!";
-    }
-    else if (containsStraight(hand)) {
+    } else if (containsStraight(hand)) {
         cout << "Straight!";
-    }
-    else if (containsThreeOfaKind(hand)) {
+    } else if (containsThreeOfaKind(hand)) {
         cout << "Three of a Kind!";
-    }
-    else if (containsTwoPair(hand)) {
+    } else if (containsTwoPair(hand)) {
         cout << "Two Pair!";
-    }
-    else if (containsPair(hand)) {
+    } else if (containsPair(hand)) {
         cout << "Pair!";
     } else {
         cout << "High Card!";
@@ -61,112 +44,69 @@ int main() {
     return 0;
 }
 
-/* 
-* Function: containsPair
-* This function checks if the poker hand contains a pair.
-*/
+void populateCardCountMap(const int hand[], map<int, int>& cardCount) {
+    for(int i = 0; i < HAND_SIZE; i++) {
+        cardCount[hand[i]]++;
+    }
+}
+
+int countOccurrences(const map<int, int>& cardCount, int count) {
+    int occurrences = 0;
+    for(auto it : cardCount) {
+        if(it.second == count) {
+            occurrences++;
+        }
+    }
+    return occurrences;
+}
+
 bool containsPair(const int hand[]) {
     map<int, int> cardCount;
-    for(int i = 0; i < HAND_SIZE; i++) {
-        cardCount[hand[i]]++;
-    }
-
-    for(auto it : cardCount)  {
-        if(it.second == 2) {
-            return true;
-        }
-    }
-    return false;
-
+    populateCardCountMap(hand, cardCount);
+    return countOccurrences(cardCount, 2) == 1;
 }
 
-/*
-* Function: containsTwoPair
-* This function checks if the poker hand contains two pairs.
-*/
 bool containsTwoPair(const int hand[]) {
     map<int, int> cardCount;
-    for(int i = 0; i < HAND_SIZE; i++) {
-        cardCount[hand[i]]++;
-    }
-
-    int pairCount = 0;
-    for(auto it : cardCount) {
-        if(it.second == 2) {
-            pairCount++;
-        }
-    }
-    return pairCount == 2;
+    populateCardCountMap(hand, cardCount);
+    return countOccurrences(cardCount, 2) == 2;
 }
 
-
-/*
-* Function: containsThreeOfaKind
-* This function checks if the poker hand contains three of a kind.
-*/
 bool containsThreeOfaKind(const int hand[]) {
     map<int, int> cardCount;
-    for(int i = 0; i < HAND_SIZE; i++) {
-        cardCount[hand[i]]++;
-    }
-
-    for(auto it : cardCount) {
-        if(it.second == 3) {
-            return true;
-        }
-    }
-    return false;
+    populateCardCountMap(hand, cardCount);
+    return countOccurrences(cardCount, 3) >= 1;
 }
 
-/*
-* Function: containsThreeOfaKind
-* This function checks if the poker hand contains a straight.
-*/
+bool containsFourOfaKind(const int hand[]) {
+    map<int, int> cardCount;
+    populateCardCountMap(hand, cardCount);
+    return countOccurrences(cardCount, 4) == 1;
+}
+
+bool containsFullHouse(const int hand[]) {
+    return containsPair(hand) && containsThreeOfaKind(hand);
+}
+
 bool containsStraight(const int hand[]) {
     map<int, int> cardCount;
-    for(int i = 0; i < HAND_SIZE; i++) {
-        cardCount[hand[i]]++;
-    }
+    populateCardCountMap(hand, cardCount);
 
     auto it = cardCount.begin();
-    int prev = it->first, count = 0;
-    for(; it != cardCount.end(); it++) {
+    int prev = it->first;
+    ++it;
+    int count = 1;
+    while(it != cardCount.end()) {
         if(it->first == prev+1) {
             count++;
         } else {
-            count = 0;
+            count = 1;
         }
-        if(count == 4) {
+        if(count == 5) {
             return true;
         }
         prev = it->first;
+        ++it;
     }
     return false;
-}
-
-/*
-* Function: containsFullHouse
-* This function checks if the poker hand contains a full house.
-*/   
-    bool containsFullHouse(const int hand[]) {
-        return containsPair(hand) && containsThreeOfaKind(hand);
-    }
-
-/*
-* Function: containsFourOfaKind
-* This function checks if the poker hand contains four of a kind.
-*/   
-    bool containsFourOfaKind(const int hand[]){
-    map<int, int> cardCount;
-    for(int i = 0; i < HAND_SIZE; i++) {
-        cardCount[hand[i]]++;
-    }
-
-    for(auto it : cardCount) {
-        if(it.second == 4) {
-            return true;
-        }
-        
-    return false;
-    }
 }
